f9ef8fd9e5ac7fd232415d96a1d82205ea062234
liu-austin <austinliu279@gmail.com>
2019-11-09 15:57:20 -0800

    completes basic requirements

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -4,6 +4,7 @@ const headers = require('./cors');
 const multipart = require('./multipartUtils');
 const validMessages = ['left', 'right', 'up', 'down'];
 const messages = require('./messageQueue.js');
+const formidable = require('formidable');
 
 // /Arrow(Up|Down|Left|Right)/
 // Path for the background image ///////////////////////
@@ -26,19 +27,37 @@ module.exports.router = (req, res, next = ()=>{}) => {
     } else {
       messages.enqueue('yo');
       res.write(messages.dequeue());
+      res.end();
     }
+  } else if (req.method === 'OPTIONS') {
+    res.writeHead(200, headers);
+    res.end();
   } else if (req.method === 'GET') {
-    fs.readFile(req.url.slice(1), function(err, data) {
-      res.writeHead(200, {'Content-Type': 'image/jpeg'});
-      res.write(data);
+    fs.readFile('.' + req.url, function(err, data) {
+      if (err) {
+        res.writeHead(404, headers);
+      } else {
+        res.writeHead(200, headers);
+        res.write(data, 'binary');
+      }
+      res.end();
     });
   } else if (req.method === 'POST') {
+    res.writeHead(201, headers);
+    // let body = [];
+    let body = Buffer.alloc(0);
+    // req.on('data', chunk => body.push(chunk));
+    req.on('data', chunk => {body = Buffer.concat([body, chunk])});
+    req.on('end', () => {
+      let buffer = Buffer.concat(body);
+      let file = multipart.getFile(buffer);
+      fs.writeFile('./background.jpg', file.data);
+    });
 
-    // res.writeHead(200, {'Content-Type': 'image/jpeg'});
-    // fs.readFile()
-  }
-  // res.write(validMessages[Math.round(Math.random()*3)]);
+    }
+    next(); // invoke next() at the end of a request to help with testing!
+  };
 
-  res.end();
-  next(); // invoke next() at the end of a request to help with testing!
-};
+
+
+
